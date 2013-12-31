284604e418969423fc55dad7a1910abeba3da089
Gary Hockin <gary@hock.in>
2013-12-31 13:58:55 +0000

    More filter/validation work

---
+++ src/UglyForm/Renderer/Label.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: garyhockin
+ * Date: 31/12/2013
+ * Time: 11:08
+ */
+
+namespace UglyForm\Renderer;
+
+
+class Label 
+{
+
+} 
---
+++ tests/UglyFormTest/Renderer/LabelTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: garyhockin
+ * Date: 31/12/2013
+ * Time: 10:32
+ */
+
+namespace FormTest\Renderer;
+
+
+class LabelTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
